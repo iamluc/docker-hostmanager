4c64a6c0888e8ba3a6e8fcafd878b85f19d5fb5a
iamluc <luc@vieillescazes.net>
2016-07-25 15:00:54 +0200

    Fix compatibilit with docker 1.12

--- src/Docker/Docker.php
+++ src/Docker/Docker.php
@@ -3,18 +3,43 @@
 namespace DockerHostManager\Docker;
 
 use Docker\Docker as DockerBase;
+use Docker\DockerClient;
+use Http\Client\HttpClient;
+use Http\Message\MessageFactory;
+use Http\Message\MessageFactory\GuzzleMessageFactory;
+use Symfony\Component\Serializer\Serializer;
 
 class Docker extends DockerBase
 {
+    /**
+     * @var HttpClient
+     */
+    private $httpClient;
+
+    /**
+     * @var MessageFactory
+     */
+    private $messageFactory;
+
+    public function __construct(HttpClient $httpClient = null, Serializer $serializer = null, MessageFactory $messageFactory = null)
+    {
+        $this->httpClient = $httpClient ?: DockerClient::createFromEnv();
+        $this->messageFactory = $messageFactory ?: new GuzzleMessageFactory();
+
+        parent::__construct($this->httpClient, $serializer, $this->messageFactory);
+    }
+
     /**
      * @param callable $callback
      */
     public function listenEvents(callable $callback)
     {
-        $stream = $this->getHttpClient()->get('/events');
+        $request = $this->messageFactory->createRequest('GET', '/events');
+        $response = $this->httpClient->sendRequest($request);
 
-        while (!$stream->getBody()->eof()) {
-            $line = \GuzzleHttp\Stream\read_line($stream->getBody());
+        $stream = $response->getBody();
+        while (!$stream->eof()) {
+            $line = \GuzzleHttp\Psr7\readline($stream);
             if (null !== ($raw = json_decode($line, true))) {
                 call_user_func($callback, new Event($raw));
             }
